ddf533209ac0db34c3dfb0eff1619d2a80ce2465
Govind <mail.govind.c@gmail.com>
2022-09-03 00:13:36 +0530

    fetchTnx done

--- bank_mock_server/bank.js
+++ bank_mock_server/bank.js
@@ -4,12 +4,34 @@ router = express.Router()
 router.post('/fetch', function(req, res){
     console.log("bank api is hit")
     console.log(req.body)
+
+    user_id = req.body.user_id
+    from = req.body.from
+    to = req.body.to
+
     const fs = require('fs');
 
     let rawdata = fs.readFileSync('./bank_mock_server/transactions.json');
-    let tnx = JSON.parse(rawdata);
+    let allTnx = JSON.parse(rawdata);
+
+    let filteredTnx = []
+
+    for(var i in allTnx){
+        tnx = allTnx[i]
+        console.log(i,tnx)
+        console.log(new Date(from) <= new Date(tnx.timestamp))
+        console.log(new Date(tnx.timestamp) <= new Date(to))
+        console.log(tnx.user_id == user_id)
+        if(
+            new Date(from) <= new Date(tnx.timestamp) && 
+            new Date(tnx.timestamp) <= new Date(to) && 
+            tnx.user_id == user_id){
+                filteredTnx.push(tnx)
+        }
+    }
+
     // console.log(tnx);
-    res.send(JSON.stringify(tnx));
+    res.send(JSON.stringify(filteredTnx));
 });
 
 module.exports = router
--- smart_spend_analyser/fetch/index.js
+++ smart_spend_analyser/fetch/index.js
@@ -12,35 +12,27 @@ function getBankEndpoint(bankId){
     return bankServer+fetchEndpoint
 }
 
-async function fetch(bankId, userId, bankUserId, from, to){
+bankId = 1
+userId = 1
+bankUserId = "abc"
+
+async function fetch(bankId, bankUserId, from, to){
     console.log(bankServer+fetchEndpoint)
 
-    // call bank mock server
     const fetchedData = await Axios({
         method: 'post',
         url: getBankEndpoint(bankId),
         headers: { Authorization: "dummy"},
         data: {
-            "customerId" : bankUserId,
+            "user_id" : bankUserId,
             "from": from,
             "to": to
         }
     })
-
     
     console.log(fetchedData)
-    // return fetchedData.data
-
-    // query local tnx in time frame
-    db = await conn()
-    response = await db.collection('transactions').find().toArray()
-    res.send(JSON.stringify(response));
-
-    // update local transactions
-
-    // db = await conn()
-    // response = await db.collection('transactions').find().toArray()
-    // res.send(JSON.stringify(response));
+    console.log('--------')
+    return fetchedData.data
 }
 
 async function getLocalTransactions(userId,bankUserId,bankId,from,to){
@@ -55,53 +47,106 @@ async function getLocalTransactions(userId,bankUserId,bankId,from,to){
                 $lt:new Date(to)
             },
         }).toArray()
-    return localTnx
+
+    indexedLocalTnx = {}
+    localTnx.forEach(tnx => {
+        indexedLocalTnx[tnx.tnxId] = tnx
+    })
+
+    return indexedLocalTnx
 }
 
-router.get('/fetch',async function(req, res){
-    const resp = await fetch(1,2,3,4,5)
-    console.log(resp,"----")
+async function updateLocalTransactions(userId, bankId, indexedLocalTnx, fetchedTnx){
+    newTnx = []
+
+    fetchedTnx.forEach(tnx => {
+        let isNew = !indexedLocalTnx[tnx.tnx_id]
+        console.log('isNew',isNew)
+        if(isNew){
+            newTnx.push({
+                "userId":userId,
+                "bankUserId":tnx.user_id,
+                "bankId":bankId,
+                "tnxId": tnx.tnx_id,
+                "timestamp": new Date(tnx.timestamp),
+                "amount": tnx.amount,
+                "tnxType": tnx.tnx_type,
+                "method": tnx.method,
+                "remark": tnx.remark,
+                "to": tnx.to
+            })
+        }
+    });
+
+    let response = []
+    if(newTnx.length > 0){
+        db = await conn()
+        console.log(newTnx)
+        // return newTnx
+        response = await db.collection('transactions').insertMany(newTnx)    
+    }
+
+    return response
+}
+
+router.post('/fetchAndUpdateLocal',async function(req, res){
+
+    from = req.body.from //|| "2022-08-31T10:00:00"
+    to = req.body.to //|| "2022-09-03T10:00:00"
+
+    // call bank mock server
+    const fetchedData = await fetch(bankId, bankUserId, from, to)
+
+    // query local tnx in time frame
+    const indexedLocalTnx = await getLocalTransactions(userId,bankUserId,bankId,from,to)
+    // console.log(indexedLocalTnx)
+
+    // update local transactions
+    const resp = await updateLocalTransactions(userId, bankId, indexedLocalTnx, fetchedData)
+
+    // console.log(resp,"----")
     res.send(JSON.stringify(resp))
-});
+});        
 
-router.get('/test',async function(req, res){
-    db = await conn()
-    response = await db.collection('transactions').insertOne(
-        {
-            "userId":1,
-            "bankUserId":"A123",
-            "bankId":1,
-            "tnxId": 11111,
-            "timestamp": new Date("2022-09-02 10:00:00"),
-            "amount": 100.5,
-            "tnxType": "debit",
-            "method": "UPI",
-            "remark": "Lunch at udupi",
-            "to": "893475934785894"
-        }).toArray()
-    res.send(JSON.stringify(response));
-});
+// router.get('/test',async function(req, res){
+//     db = await conn()
+//     response = await db.collection('transactions').insertOne(
+//         {
+//             "userId":1,
+//             "bankUserId":"A123",
+//             "bankId":1,
+//             "tnxId": 11111,
+//             "timestamp": new Date("2022-09-02 10:00:00"),
+//             "amount": 100.5,
+//             "tnxType": "debit",
+//             "method": "UPI",
+//             "remark": "Lunch at udupi",
+//             "to": "893475934785894"
+//         }).toArray()
+//     res.send(JSON.stringify(response));
+// });
 
-router.get('/query',async function(req, res){
+router.get('/getTnx',async function(req, res){
     db = await conn()
     localTnx = await db.collection('transactions').find(
-        {
-            "userId":1,
-            "bankUserId":"A123",
-            "bankId":1,
-            "timestamp": {
-                $gt:new Date("2022-09-02 09:00:00"),
-                $lt:new Date("2022-09-02 10:00:01")
-            },
-        }).toArray()
+        // {
+        //     "userId":1,
+        //     "bankUserId":"A123",
+        //     "bankId":1,
+        //     "timestamp": {
+        //         $gt:new Date("2022-09-02 09:00:00"),
+        //         $lt:new Date("2022-09-02 10:00:01")
+        //     },
+        // }
+    ).toArray()
     res.send(JSON.stringify(localTnx));
 });
 
-router.post('/banklink',async function(req, res){
-    // custom logic to prevent duplicate entries
-    db = await conn()
-    response = await db.collection('banklink').insertOne({"userId":1,"accountNo":req.body.accountNo,"bankId":req.body.bankId})
-    res.send(JSON.stringify(response));
-});
+// router.post('/banklink',async function(req, res){
+//     // custom logic to prevent duplicate entries
+//     db = await conn()
+//     response = await db.collection('banklink').insertOne({"userId":1,"accountNo":req.body.accountNo,"bankId":req.body.bankId})
+//     res.send(JSON.stringify(response));
+// });
 
 module.exports = router
